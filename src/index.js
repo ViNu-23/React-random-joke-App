df07c7c55fd3d29f72de05c690e5faee12aa465a
ViNu-23 <vijayvinu46@gmail.com>
2023-12-16 16:18:50 +0530

    New

--- src/index.js
+++ src/index.js
@@ -1,17 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useEffect, useState } from "react";
+import "./index.css";
+import ReactDOM from "react-dom/client";
+import axios from "axios";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function Main() {
+  const [joke, setJoke] = useState("");
+  const [buttonColor, setButtonColor] = useState("#3498db");
+  async function getData() {
+    try {
+      const response = await axios.get("https://icanhazdadjoke.com/", {
+        headers: {
+          Accept: "application/json",
+        },
+      });
+      setJoke(response.data.joke);
+    } catch (error) {
+      console.log(`API Fetch error ${error}`);
+    }
+  }
+  console.log(joke);
+  useEffect(() => {
+    getData();
+  }, []);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  function handleClick() {
+    const randomButtonColor = getRandomColor();
+    setButtonColor(randomButtonColor);
+    getData();
+  }
+  function getRandomColor() {
+    return "#" + Math.floor(Math.random() * 16777215).toString(16);
+  }
+  return (
+    <div className="container">
+      <h1 style={{ color: buttonColor }}>Try Not to Laugh 😂</h1>
+      <p className="joke-text">{joke}</p>
+      <div className="button">
+        <button
+          className="btn new-joke-btn"
+          onClick={handleClick}
+          style={{ backgroundColor: buttonColor }}
+        >
+          New Joke
+        </button>
+      </div>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Main />);
